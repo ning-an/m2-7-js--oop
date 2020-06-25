f8c1de8157a47ef01b90327a26042f0b8bf0d8dc
Ning An <ning.an@outlook.com>
2020-06-25 12:25:41 -0400

    Exercise2.1-solution

--- workshop/2-Books/exercise-2.1.js
+++ workshop/2-Books/exercise-2.1.js
@@ -15,6 +15,18 @@
 //
 // Console.log them to verify that all is working.
 
-class Book {}
+class Book {
+    constructor(title, genre, author, isRead) {
+        this.title = title;
+        this.genre = genre;
+        this.author = author;
+        this.isRead = isRead || false;
+    }
+}
+const book1 = new Book('To Kill a Mockingbird', 'novel', 'Harper Lee', true);
+const book2 = new Book('ALL THE LIGHT WE CANNOT SEE: A NOVEL', 'novel', 'Anthony Doerr', false);
+const book3 = new Book('THE LIES THAT BIND', 'fiction', 'Emily Giffin');
+const book4 = new Book('TRUTH: A BRIEF HISTORY OF TOTAL BULLSH*T', 'non-fiction', 'Tom Phillips');
+const book5 = new Book('Beautiful Disaster', 'romance', 'Jamie McGuire', true);
 
 console.log(book1, book2, book3, book4, book5);
